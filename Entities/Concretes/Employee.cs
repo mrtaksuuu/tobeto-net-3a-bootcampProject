658f9c114c1bf363c3de2db53c6d5067af8b8a56
ruhim <ruhim@DESKTOP-43RLGVU>
2024-02-16 12:03:00 +0300

    Add project files.

---
+++ Entities/Concretes/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Employee
+    {
+        public string Position { get; set; }
+        public int UserId { get; set; }
+
+
+        public Employee() 
+        {
+        
+        }
+
+        public Employee(string position, int userId)
+        {
+            Position = position;
+            UserId = userId;
+        }
+    }
+}
---
+++ Entities/Concretes/Instructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Instructor
+    {
+        public string CompanyName { get; set; }
+
+        public Instructor()
+        {
+
+        }
+    }
+}
